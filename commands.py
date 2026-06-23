2c0be9a5c1d0026a9cea8d91828011a2c7ea8409
parthnikamm26 <parth.nikamm1126@gmail.com>
2026-06-24 00:01:47 +0530

    Implement action-target command parser

--- commands.py
+++ commands.py
@@ -1,14 +1,17 @@
-def greet_user():
-    print("Hey, I'm Jarvis!")
-
 def play_music():
     print("Playing music...")
 
 def open_spotify():
     print("Opening Spotify...")
+   
+def search_youtube():
+    print("Searching Youtube...")
+
+def tell_time():
+    print("The time is...")
 
 def exit_program():
     print("GOODBYE!")
     return False
-    
+
 
--- main.py
+++ main.py
@@ -1,28 +1,57 @@
-from commands import greet_user
 from commands import play_music
 from commands import open_spotify
+from commands import search_youtube
+from commands import tell_time
 from commands import exit_program
 
 running = True
+
+actions = [
+    "play",
+    "open",
+    "search",
+    "tell"
+]
+
+targets = [
+    "music",
+    "spotify",
+    "youtube",
+    "time"
+]
+
 command_map = {
-    "hello": greet_user,
-    "play music": play_music,
-    "open spotify": open_spotify,
-    "exit program": exit_program
+    ("play", "music"): play_music,
+    ("open", "spotify"): open_spotify,
+    ("search", "youtube"): search_youtube,
+    ("tell", "time"): tell_time,
 }
 
 while running:
+    action = None
+    target = None
     commands = input("What can I do for you?")
-    commands = commands.strip().lower()
-    if commands in command_map:
-        action = command_map[commands]
-        result = action()
+    commands = commands.strip().lower().split()
+    for word in commands:
+        if word in actions:
+            action = word
+        if word in targets:
+            target = word
+    command_key = (action, target)
+    if command_key in command_map:
+        action_function = command_map[command_key]
+        result = action_function()
+
         if result == False:
-            running = False
-        else:
-            running = True
+            running = False 
+    
     else:
-        print("command not found")
+        print("Command not found!")
+        
+
+        
+
+
 
     
 
